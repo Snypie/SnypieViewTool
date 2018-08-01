c0a03512060da3682db148c6b4c140df642070f7
Snypie <doworkmarine@gmail.com>
2018-08-01 19:40:43 +0900

    First gem

---
+++ SmypieViewTool/lib/SmypieViewTool.rb
@@ -0,0 +1,6 @@
+require "SmypieViewTool/version"
+require "SnypieViewTool/renderer"
+
+module SmypieViewTool
+  # Your code goes here...
+end
---
+++ SmypieViewTool/lib/SmypieViewTool/version.rb
@@ -0,0 +1,3 @@
+module SmypieViewTool
+  VERSION = "0.1.0"
+end
